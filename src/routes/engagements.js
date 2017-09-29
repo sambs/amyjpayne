f6942c9abe8bd19a6f4517446148bd8f1e75d371
Sam Barnes <sam@avantrural.com>
2017-09-29 07:43:13 +0100

    Add pagination to engagements

--- src/routes/engagements.js
+++ src/routes/engagements.js
@@ -1,28 +1,67 @@
-import { route, routes } from '../util/routes'
+import { partition, compose, splitEvery, map, evolve, reverse, sortBy, prop, over, lensIndex } from 'ramda'
+import classNames from 'classnames'
+import { route, routes, paged } from '../util/routes'
 import Layout from '../components/layout'
 import engagements from '../../data/engagements.json'
 
-const render = route(() =>
+const now = new Date()
+
+const [ past, upcoming ] = compose(
+  over(lensIndex(0), compose(splitEvery(10), reverse)),
+  partition(e => e.date < now),
+  sortBy(prop('date')),
+  map(evolve({ date: d => new Date(d) })),
+)(engagements)
+
+const render = route(({ path, items, upcoming, pages }) => console.log(path, pages) ||
   <Layout>
-    <ul className="engagements">
-      {engagements.map(({ id, title, dateDescription, location, description, link }) => (
-        <li key={id} className="engagement">
-          <h3 className="engagement-title">{title}</h3>
-          <div className="engagement-details">
-            <span className="engagement-dtstart">{dateDescription}</span>
-            - <span className="engagement-location">{location}</span>
-          </div>
-          <div className="engagement-description">
-            <p>{description}</p>
-          </div>
-          {link && <a className="engagement-link" href={link}>More info</a>}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <div className="engagement-nav">
+        <a
+          className={classNames({ 'engagement-nav-item': true, active: upcoming })}
+          href="/engagements/"
+          children="Upcoming"
+        />
+        <a
+          className={classNames({ 'engagement-nav-item': true, active: !upcoming })}
+          href="/engagements/past/"
+          children="Past"
+        />
+        {pages && (
+          <span>
+            {pages.map(({ number, path: pagePath }) => (
+              <a
+                className={classNames({ 'engagement-nav-item': true, active: path == pagePath })}
+                href={pagePath}
+                children={number}
+              />
+            ))}
+          </span>
+        )}
+      </div>
+      <ul className="engagements">
+        {map(
+          ({ id, title, dateDescription, location, description, link }) => (
+            <li key={id} className="engagement">
+              <h3 className="engagement-title">{title}</h3>
+              <div className="engagement-details">
+                <span className="engagement-dtstart">{dateDescription}</span>
+                - <span className="engagement-location">{location}</span>
+              </div>
+              <div className="engagement-description">
+                <p>{description}</p>
+              </div>
+              {link && <a className="engagement-link" href={link}>More info</a>}
+            </li>
+          ),
+          items
+        )}
+      </ul>
+    </div>
   </Layout>
 )
 
 export default routes([
-  [ '/', render ],
-  [ '/past', render ],
+  [ '/', render, { items: upcoming, upcoming: true } ],
+  [ '/past', paged(render, past, { upcoming: false }) ],
 ])
--- src/util/routes.js
+++ src/util/routes.js
@@ -1,18 +1,30 @@
-import { pipe, map, reduce, merge, compose } from 'ramda'
+import { pipe, map, reduce, merge, mergeAll, compose, addIndex, assoc } from 'ramda'
 import { join } from 'path'
 import render from './render'
 
-const route = fn => props => 
+const mapIndexed = addIndex(map)
+
+const route = fn => props =>
   ({ [props.path]: compose(render, fn)(props) })
 
-const routes = routes => props => 
+const routes = routes => props =>
   pipe(
-    map(([ subPath, fn ]) => {
+    map(([ subPath, fn, extraProps ]) => {
       const path = join(props.path, subPath)
-      const subProps = merge(props, { path })
+      const subProps = mergeAll([ props, { path }, extraProps ])
       return fn(subProps)
     }),
     reduce(merge, {}),
   )(routes)
 
-export { route, routes }
+const paged = (fn, pages) => props =>
+  pipe(
+    mapIndexed((items, index) => ({ items, index, number: index + 1 })),
+    map(page => assoc('path', `${props.path}/${page.index == 0 ? '' : `${page.number}/` }`, page)),
+    pages => map(assoc('pages', pages), pages),
+    map(merge(props)),
+    map(props => fn(props)),
+    reduce(merge, {}),
+  )(pages)
+
+export { route, routes, paged }
